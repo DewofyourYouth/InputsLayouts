f969c57ee31378aa8fa92ace4fc87e9565402710
Jacob Shore <jshore@datalign.net>
2021-02-22 22:40:42 +0200

    tomato colored

--- src/components/ShowNameASCIICodes.js
+++ src/components/ShowNameASCIICodes.js
@@ -1,35 +1,38 @@
 export default function ShowNameASCIICodes({ name, petName, species }) {
-  // This is where I would fetch some data for the reports
-  // based on whatever props I recieved
-  return (
-    <main>
-      <h3>
-          Hello <small>(from ShowNameASCIICodes)</small> <span style={{ color: "purple" }}>{name}</span>!
-      </h3>
-      <h5>ASCII Codes</h5>
-      <p>
-        {name
-          .split("")
-          .filter((x) => x !== " ")
-          .map((x, i) => (
-            <span key={i}>
+    // This is where I would fetch some data for the reports
+    // based on whatever props I recieved
+    return (
+        <main>
+            <h3>
+                Hello <small>(from ShowNameASCIICodes)</small>{" "}
+                <span style={{ color: "tomato" }}>{name}</span>!
+            </h3>
+            <h5>ASCII Codes</h5>
+            <p>
+                {name
+                    .split("")
+                    .filter((x) => x !== " ")
+                    .map((x, i) => (
+                        <span key={i}>
               {x}: {x.charCodeAt(0)},&nbsp;
             </span>
-          ))}
-      </p>
-      {petName && species && (
-        <OptionalPetSection petName={petName} species={species}/>
-      )}
-    </main>
-  );
+                    ))}
+            </p>
+            {petName && species && (
+                <OptionalPetSection petName={petName} species={species} />
+            )}
+        </main>
+    );
 }
 
 function OptionalPetSection(props) {
-    return <div>
-        <hr/>
-        <h2>Optional Pet Section:</h2>
-        <h5>
-            The pet is {props.petName} the {props.species}
-        </h5>
-    </div>;
+    return (
+        <div>
+            <hr />
+            <h2>Optional Pet Section:</h2>
+            <h5>
+                The pet is {props.petName} the {props.species}
+            </h5>
+        </div>
+    );
 }
