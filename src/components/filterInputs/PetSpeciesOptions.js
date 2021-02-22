2377d01325d2717138d76964fbfe8a8a59e86364
Jacob Shore <jshore@datalign.net>
2021-02-22 22:36:53 +0200

    format options

--- src/components/filterInputs/PetSpeciesOptions.js
+++ src/components/filterInputs/PetSpeciesOptions.js
@@ -1,23 +1,46 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default function PetSpeciesOptions(props) {
     const [optionState, setOptionState] = useState("");
     const handleChange = (species) => {
         setOptionState(species);
-        props.handleUpdate({species});
+        props.handleUpdate({ species });
     };
 
-    const petSpeciesOptions = ['cat', 'pangolin', 'rabbit', 'monkey', 'dragon', 'dog', 'lizard', 'snake', 'gerbil', 'parrot', 'mouse', 'human', 'falcon', 'goose', 'rat', 'chinchilla']
+    const petSpeciesOptions = [
+        "cat",
+        "pangolin",
+        "rabbit",
+        "monkey",
+        "dragon",
+        "dog",
+        "lizard",
+        "snake",
+        "gerbil",
+        "parrot",
+        "mouse",
+        "human",
+        "falcon",
+        "mongoose",
+        "honey badger",
+        "goose",
+        "rat",
+        "chinchilla"
+    ];
     return (
         <select
             name={"species"}
             value={optionState}
             onChange={(e) => {
-                handleChange(e.target.value)
+                handleChange(e.target.value);
             }}
         >
             <option value={""}>Choose a species</option>
-            {petSpeciesOptions.sort().map((species, i) => (<option key={i} value={species}>{species}</option>))}
+            {petSpeciesOptions.sort().map((species, i) => (
+                <option key={i} value={species}>
+                    {species}
+                </option>
+            ))}
         </select>
     );
 }
